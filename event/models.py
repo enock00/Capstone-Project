dd1fc6fece086a8b8f13828f24ef8bddb7c4f74b
Enock <enockzy54@gmail.com>
2025-08-26 10:26:14 +0300

    Adding Comments Model

--- event/models.py
+++ event/models.py
@@ -20,3 +20,13 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="comments")
+    user =  models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"comment by {self.user.username} on {self.event.title} "
+     
